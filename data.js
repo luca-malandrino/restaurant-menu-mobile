416634bffbcf4ac4d000b566520faef717df0d56
Luca Malandrino <malandrinoluca3@gmail.com>
2025-03-17 15:28:44 +0100

    Completed

--- data.js
+++ data.js
@@ -4,20 +4,23 @@ export const menuArray = [
       ingredients: ["pepperoni", "mushrom", "mozzarella"],
       id: 0,
       price: 14,
-      emoji: "🍕"
+      emoji: "🍕",
+      units: 0
   },
   {
       name: "Hamburger",
       ingredients: ["beef", "cheese", "lettuce"],
       price: 12,
       emoji: "🍔",
-      id: 1
+      id: 1,
+      units: 0
   },
-      {
+  {
       name: "Beer",
       ingredients: ["grain, hops, yeast, water"],
       price: 12,
       emoji: "🍺",
-      id: 2
+      id: 2,
+      units: 0
   }
 ]
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { menuArray as menuItems } from './data.js'
 
-let orderedItems = []
 const renderMenuItems = () => {
   let feed = ''
 
@@ -13,7 +12,7 @@ const renderMenuItems = () => {
           <p class="ingredients">${item.ingredients.join(', ')}</p>
           <p class="price">$${item.price}</p>
         </div>
-        <button class="add-button" data-name="${item.name}">+</button>
+        <button class="add-button" data-id="${item.id}">+</button>
       </div>
     `
     document.querySelector('.container').innerHTML = feed
@@ -23,10 +22,12 @@ const renderMenuItems = () => {
 const renderCheckout = (arr) => {
   let total = 0
   document.querySelector('tbody').innerHTML = arr.map(item => {
-    total += item[0].price
+    total += item.price * item.units
     return /*html*/ `<tr>
-      <th>${item[0].name} <button>Remove</button></th>
-      <td>$${item[0].price}</td>
+      <th class="units">${item.units}</th>
+      <th class="name">${item.name} <button class="remove-btn" data-id="${item.id}">Remove</button>
+      </th>
+      <td class="ordered-price">$${item.price * item.units}</td>
     </tr>`
   }).join('')
 
@@ -35,13 +36,36 @@ const renderCheckout = (arr) => {
 
 renderMenuItems()
 
-document.querySelector('main').addEventListener('click', e => {
+document.addEventListener('click', e => {
   if(e.target.textContent === '+') {
-    orderedItems.push(menuItems.filter(item => item.name === e.target.dataset.name))
-
-    renderCheckout(orderedItems)
-    document.querySelector('footer').style.display = 'block' 
-  } else if(e.target.textContent === 'remove') {
-    
+    menuItems.filter(item => item.id === Number(e.target.dataset.id))[0].units += 1
+    renderCheckout(menuItems.filter(item => item.units))
+    document.querySelector('footer').style.display = 'block'
+  } else if(e.target.classList.contains('remove-btn')) {
+    menuItems.filter(item => item.id === Number(e.target.dataset.id))[0].units -= 1
+    renderCheckout(menuItems.filter(item => item.units))
+    document.querySelector('footer').style.display = menuItems.filter(item => item.units).length === 0 ? 'none' : 'block'
+  } else if(e.target.id === 'complete-btn') {
+    document.getElementById('modal').style.display = 'block'
   }
+})
+
+document.getElementById('modal').addEventListener('submit', e => {
+  e.preventDefault()
+
+  e.target.style.display = 'none'
+  document.getElementById('message').style.display = 'block'
+  menuItems.forEach(item => item.units = 0)
+  document.querySelector('footer').style.display = 'none'
+
+  setTimeout(() => document.getElementById('rating-modal').style.display = 'block', 3000)
+})
+
+document.querySelectorAll('i').forEach(icon => {
+  icon.addEventListener('click', e => {
+    document.querySelectorAll('i').forEach(icon => icon.style.color = '#c4c4c4')
+    for(let i = Number(e.target.id); i >= 0; i--) {
+      document.getElementById(i).style.color = '#ffdd00'
+    }
+  })
 })
